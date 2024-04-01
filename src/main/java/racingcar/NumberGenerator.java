962be552c3afd0908520497274100b23ab0c3461
jenna.me <inthejim24@gmail.com>
2024-04-01 14:25:42 +0900

    refactor: 코드 개선, 상수화

--- src/main/java/racingcar/NumberGenerator.java
+++ src/main/java/racingcar/NumberGenerator.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class NumberGenerator {
     private static final int NUMBER_RANGE = 9;
+    private final Random rand = new Random();
 
     public int generateNumber() {
-        Random rand = new Random();
         return rand.nextInt(NUMBER_RANGE + 1);
     }
 }
