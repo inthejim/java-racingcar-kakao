c787ab3b8bac8770e81643b1f63c8cfe6f2a5c97
jenna.me <inthejim24@gmail.com>
2024-04-01 14:47:37 +0900

    refactor: 테스트 코드 개선

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -5,55 +5,62 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("문자열 계산기 단위 테스트")
 public class StringCalculatorTest {
 
-    StringCalculator calculator;
+    private StringCalculator calculator;
 
     @BeforeEach
     void setUp() {
         calculator = new StringCalculator();
     }
 
-    @Test
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {""})
     @DisplayName("빈 문자열 또는 null을 입력할 경우 0을 반환한다.")
-    void 빈문자열_혹은_null() {
-        assertThat(calculator.calculate("")).isEqualTo(0);
-        assertThat(calculator.calculate(null)).isEqualTo(0);
+    void 빈문자열_혹은_null(String input) {
+        assertThat(calculator.calculate(input)).isEqualTo(0);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3"})
     @DisplayName("숫자 하나를 입력할 경우 해당 숫자를 반환한다.")
-    void 숫자하나() {
-        assertThat(calculator.calculate("1")).isEqualTo(1);
+    void 숫자하나(String input) {
+        assertThat(calculator.calculate(input)).isEqualTo(Integer.parseInt(input));
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"1:2:3", "1,2,3", "1,2:3"})
     @DisplayName("기본 구분자를 기준으로 분리해 합을 계산한다.")
-    void 기본구분자_분리() {
-        assertThat(calculator.calculate("1,2")).isEqualTo(3);
-        assertThat(calculator.calculate("1,2,3")).isEqualTo(6);
-        assertThat(calculator.calculate("1,2:3")).isEqualTo(6);
+    void 기본구분자_분리(String input) {
+        assertThat(calculator.calculate(input)).isEqualTo(6);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1,2;3"})
     @DisplayName("커스텀 구분자를 기준으로 분리해 합을 계산한다.")
-    void 커스텀구분자_분리() {
-        assertThat(calculator.calculate("//;\n1,2;3")).isEqualTo(6);
+    void 커스텀구분자_분리(String input) {
+        assertThat(calculator.calculate(input)).isEqualTo(6);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"ㄱ,1,2", "1,ㄱ,2", "ㄱ"})
     @DisplayName("숫자 이의외 값을 전달할 경우 RuntimeException 예외를 던진다.")
-    void 숫자_이외의_값() {
+    void 숫자_이외의_값(String input) {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> calculator.calculate("ㄱ,1,2"));
+                .isThrownBy(() -> calculator.calculate(input));
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "1,-3,2"})
     @DisplayName("음수 값을 전달할 경우 RuntimeException 예외를 던진다.")
-    void 음수_값() {
+    void 음수_값(String input) {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> calculator.calculate("1,-3,2"));
+                .isThrownBy(() -> calculator.calculate(input));
     }
 }
